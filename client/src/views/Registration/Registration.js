26454db60bfa1595806af43597f5565a955decbe
vaishnavihole <vaishnavihole1@gmail.com>
2023-11-21 13:23:53 +0530

    api integration added

--- client/src/views/Registration/Registration.js
+++ client/src/views/Registration/Registration.js
@@ -1,9 +1,54 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './Registration.css'
 import Footer from '../../component/Footer/Footer';
 import Navbar from '../../component/Navbar/Navbar';
 
 function Registration() {
+  const [userName, setUserName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const register = async () => {
+    if(!userName){
+      alert("UserName is required");
+      return;
+    }
+
+    if(!email){
+      alert("Email is required");
+      return;
+    }
+
+    if(!password){
+      alert("Password is required");
+      return;
+    }
+
+    
+    const response = await axios.post("/signup", {
+      userName:  userName,
+      email: email,
+      password: password,
+    })
+
+    alert(response?.data?.message);
+
+    if(response?.data?.success){
+      window.location.href = "/users";
+    }
+  };
+
+  useEffect(()=>{
+    const storageUser = JSON.parse(localStorage.getItem("user") || '{}');
+
+    if(storageUser?.email){
+      alert("You are already logged in!");
+      window.location.href = "/";
+    }
+
+  }, [])
+
 
   return (
     <>
@@ -11,15 +56,20 @@ function Registration() {
       <div className="registration-container">
         <h2>Registration</h2>
         <form className="registration-form">
+
           <div className="form-group">
             <label htmlFor="username">Username:</label>
             <input
               type="text"
               id="username"
               name="username"
-              placeholder='username'
+              value={userName}
+              placeholder='Enter username'
               required
               className="form-control"
+              onChange={(e) => {
+                setUserName(e.target.value);
+              }}
             />
           </div>
 
@@ -29,9 +79,13 @@ function Registration() {
               type="email"
               id="email"
               name="email"
-              placeholder='email'
+              placeholder='Enter email'
+              value={email}
               required
               className="form-control"
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
             />
           </div>
 
@@ -41,23 +95,21 @@ function Registration() {
               type="password"
               id="password"
               name="password"
+              value={password}
               placeholder='password'
               required
               className="form-control"
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
             />
           </div>
 
-          <div className="form-group">
-            <label htmlFor="isVerified">Is Verified:</label>
-            <input
-              type="checkbox"
-              id="isVerified"
-              name="isVerified"
-              className="checkbox"
-            />
-          </div>
 
-          <button type="submit" className="submit-button">Register</button>
+          <button type="button" 
+          className="submit-button"  
+          onClick={register}>Register
+          </button>
         </form>
       </div>
       <Footer />
